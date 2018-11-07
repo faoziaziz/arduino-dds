688c2fb1e6a09d3ffd369053bdca513cd4c0bde0
Aziz Amerul Faozi <admin@labseni.com>
2018-11-08 00:39:21 +0700

    update kode

--- arduino-dds.ino
+++ arduino-dds.ino
@@ -4,15 +4,37 @@
 */
 
 #include "support.h"
+#include <SPI.h>
 
-Test tes;
-void setup() {
-  pinMode(tes.pinBlink, OUTPUT);
-}
+int ss=10;
+int del=200;
 
-void loop() {
-  digitalWrite(tes.pinBlink, HIGH);
-  delay(500);
-  digitalWrite(tes.pinBlink, LOW);
-  delay(500);
-}
+void setup()
+{
+	pinMode(ss, OUTPUT);
+	SPI.begin();
+	SPI.setBitOrder(MSBFIRST);
+	
+	}
+	
+void setValue(int value)
+{
+	digitalWrite(ss, LOW);
+	SPI.transfer(0);
+	SPI.transfer(value);
+	digitalWrite(ss, HIGH);
+	
+	}
+void loop()
+{
+	for (int a=0; a<256; a++)
+	{
+		setValue(a);
+		delay(del);
+		}
+	for (int a=255; a>=0; --a)
+	{
+		setValue(a);
+		delay(del);
+		}
+	}
