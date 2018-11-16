a9ac746fae1e4a0545daedc2558cb51534df2dad
Aziz Amerul Faozi <admin@labseni.com>
2018-11-16 18:59:46 +0700

    assek

--- buat.sh
+++ buat.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 arduino-cli compile --fqbn arduino:avr:uno ../arduino-dds
-arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno ../arduino-dds
+#arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno ../arduino-dds
